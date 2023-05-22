e9101567465ec6fa04877e26bb2f858267bc7d30
hieuthahn <hieuthahn@gmail.com>
2023-05-22 22:02:40 +0700

    Fix react-hydration-error

--- src/components/atoms/Tag.tsx
+++ src/components/atoms/Tag.tsx
@@ -1,19 +1,20 @@
 import { Route } from '@/lib';
-import Link from 'next/link'
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 interface TagProps {
     name: string;
 }
 
-const Tag = ({name} : TagProps) => {
-  return (
-    <Link
-        href={Route.tag.get(name)}
-        className="bg-opacity-80 dark:!bg-opacity-60 inline-block rounded-md px-1.5 py-0.5 font-medium transition-colors bg-gray-100 text-gray-700 hover:text-black disabled:text-gray-300 dark:bg-gray-700 dark:text-gray-200 dark:hover:text-white dark:disabled:bg-gray-600 focus:outline-none mr-2"
-        tabIndex={-1}
-    >
-        {name}
-    </Link>
-  )
-}
+const Tag = ({ name }: TagProps) => {
+    return (
+        <object type="owo/uwu">
+            <Link
+                href={Route.tag.get(name)}
+                className="inline-block bg-opacity-80 dark:!bg-opacity-80 rounded-md px-1.5 py-0.5 font-medium transition-colors bg-gray-100 text-gray-700 hover:text-black disabled:text-gray-300 dark:bg-gray-700 dark:text-gray-200 dark:hover:text-white dark:disabled:bg-gray-600 focus:outline-none mr-2">
+                {name}
+            </Link>
+        </object>
+    );
+};
 
-export default Tag
+export default Tag;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html className="scroll-smooth" lang="vi">
-      <Head />
-      <body className="transition-colors dark:bg-dark">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html className="scroll-smooth" lang="vi">
+            <meta name="format-detection" content="telephone=no, date=no, email=no, address=no" />
+            <Head />
+            <body className="transition-colors dark:bg-dark">
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
